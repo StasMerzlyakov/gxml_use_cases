802e9158a6060b0d281c01c66dab86b192573552
user <you@example.com>
2022-09-25 12:40:39 +0400

    TODO - восстановление объекта

--- api/factory_test.go
+++ api/factory_test.go
@@ -38,3 +38,19 @@ func TestValidator2(t *testing.T) {
 	err = xsdValidator.Validate()
 	assert.NotNil(t, err)
 }
+
+func TestValidator3(t *testing.T) {
+	file, err := os.Open("../test_files/xml/card_response_3.xml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+	r := bufio.NewReader(file)
+
+	xsdValidator, err := NewXsdValidator(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = xsdValidator.Validate()
+	assert.Nil(t, err)
+}
